6a882f1c8c832ec26e35c0e1b7b6e9b2b14a5805
Sebastian Krüger <2pi_r2@gmx.de>
2022-03-11 20:29:37 +0100

    add enoding

--- encoding.ts
+++ encoding.ts
@@ -1,31 +1,32 @@
 export { base64Decode } from './deps.ts'
 
+const encoder = new TextEncoder()
+
 export function quotedPrintableEncode(data: string, encLB = false) {
-  const encodedData = data.split('').map((ch, i) => {
+  const encodedData = Array.from(data).map((ch, i) => {
     // For each char check decoding
-    const code = ch.charCodeAt(0)
+    const encodedChar = encoder.encode(ch)
 
-    if(!encLB && (code === 10 || code === 13)) return ch
-    if(code === 9) return ch
-    if(code > 32 && code <= 126 && code !== 61) return ch
+    if(encodedChar.length === 1) {
+      const code = encodedChar[0]
 
-    if(code === 32) {
-      // When LB after space escape it!
-      if(!encLB && (data[i+1] === '\r' || data[i+1] === '\n')) {
-        return '=20'
+      if(!encLB && (code === 10 || code === 13)) return ch
+      if(code === 9) return ch
+      if(code > 32 && code <= 126 && code !== 61) return ch
+
+      if(code === 32) {
+        // When LB after space escape it!
+        if(!encLB && (data[i+1] === '\r' || data[i+1] === '\n')) {
+          return '=20'
+        }
+        return ' '
       }
-      return ' '
     }
-
-    const hex = code.toString(16)
-
-    const bytes = hex.length / 2
 
     let enc = ''
-
-    for (let i = 0; i < bytes; i++) {
-      enc+= `=${hex.slice(2*i, 2*i+2)}`
-    }
+    encodedChar.forEach(i => {
+      enc += `=${i.toString(16)}`
+    })
 
     return enc
   }).join('')
@@ -49,25 +50,3 @@ export function quotedPrintableEncode(data: string, encLB = false) {
   return ret
 }
 
-
-export function quotedPrintableDecode(str: string) {
-  const removedLB = str.replaceAll('=\r\n', '')
-
-  const enc = removedLB.split('=').map((s, i) => {
-    if(i===0) return s
-
-    const first = s.slice(0, 2)
-    const rest = s.slice(2)
-
-    const a = new Uint8Array(parseInt(first, 16))
-
-    const text = new TextDecoder()
-
-    text.decode()
-
-
-    return String.fromCharCode(parseInt(first, 16)) + rest
-  }).join('')
-
-  return enc
-}
