070166724dacbbd7d9a99843e98793a4ccf9e0e3
Sebastian Krüger <2pi_r2@gmx.de>
2022-03-23 18:33:24 +0100

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
 export {
   BufReader,
   BufWriter,
-} from "https://deno.land/std@0.129.0/io/buffer.ts";
-export { TextProtoReader } from "https://deno.land/std@0.129.0/textproto/mod.ts";
-export { decode as base64Decode } from "https://deno.land/std@0.129.0/encoding/base64.ts";
+} from "https://deno.land/std@0.130.0/io/buffer.ts";
+export { TextProtoReader } from "https://deno.land/std@0.130.0/textproto/mod.ts";
+export { decode as base64Decode } from "https://deno.land/std@0.130.0/encoding/base64.ts";
