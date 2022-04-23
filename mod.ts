4445d229b5af2df16fe3d30896f9f4bad850a84f
mathe42 <2pi_r2@gmx.de>
2022-04-23 20:47:56 +0200

    cleanup mod.ts

--- mod.ts
+++ mod.ts
@@ -1,6 +1,4 @@
 export type { SendConfig } from "./config.ts";
 export { createSMTPConnection } from './entry.ts'
-export type {ClientOptions} from './entry.ts'
-export { SMTPWorker, SMTPWorkerPool } from './pool.ts'
-export { SmtpClient } from "./smtp.ts";
-export { quotedPrintableEncode } from "./encoding.ts";
+export type { ClientOptions } from './entry.ts'
+
