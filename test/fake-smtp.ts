77d69007c8dcc2a1b63e1fa38b8e9f4cf733468f
mathe42 <2pi_r2@gmx.de>
2022-04-24 16:02:42 +0200

    add better types

--- test/fake-smtp.ts
+++ test/fake-smtp.ts
@@ -1,5 +1,23 @@
-// deno-lint-ignore no-explicit-any
-export function getEmails(): Promise<any[]> {
+interface MailAdresses {
+  value: { address: string; name: string }[];
+  html: string;
+  text: string;
+}
+
+export function getEmails(): Promise<
+  {
+    // deno-lint-ignore no-explicit-any
+    attachments: any[];
+    headerLines: { key: string; line: string }[];
+    text: string;
+    textAsHtml: string;
+    subject: string;
+    date: string;
+    to: MailAdresses;
+    from: MailAdresses;
+    html: false | string;
+  }[]
+> {
   return fetch("http://localhost:1080/api/emails", {
     method: "get",
   }).then((v) => v.json());
