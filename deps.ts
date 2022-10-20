48801dcc6eb5a7217c8a0b48e0b092f7d9478d09
GitHub <noreply@github.com>
2022-10-20 19:54:26 +0000

    Update dependencies

--- deps.ts
+++ deps.ts
@@ -1,9 +1,9 @@
 export {
   BufReader,
   BufWriter,
-} from "https://deno.land/std@0.142.0/io/buffer.ts";
-export { TextProtoReader } from "https://deno.land/std@0.142.0/textproto/mod.ts";
+} from "https://deno.land/std@0.160.0/io/buffer.ts";
+export { TextProtoReader } from "https://deno.land/std@0.160.0/textproto/mod.ts";
 export {
   decode as base64Decode,
   encode as base64Encode,
-} from "https://deno.land/std@0.142.0/encoding/base64.ts";
+} from "https://deno.land/std@0.160.0/encoding/base64.ts";
